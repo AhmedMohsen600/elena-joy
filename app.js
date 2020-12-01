3ae21f3ec23fd648eb7883d8d32c61016f1f7689
AhmedMohsen <55211914+AhmedMohsen600@users.noreply.github.com>
2020-12-01 16:50:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll(" a");
+const html = document.querySelector('html');
 
 burger.addEventListener('click', () =>{
     nav.classList.toggle("nav-open");
     burger.classList.toggle("toggle");
+     html.classList.toggle('no-scroll');
 });
 links.forEach(link => {
 link.addEventListener("click", ()=>{
